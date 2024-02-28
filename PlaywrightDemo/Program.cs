44be20aa99e037c614c43b9562522f8f1b6aacd9
shkinosh <shkinosh@SHKINOSH-PC02>
2024-02-28 23:41:26 +0900

    Add project files.

---
+++ PlaywrightDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+
+namespace PlaywrightDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://playwright.dev/dotnet");
+            await page.ScreenshotAsync(new PageScreenshotOptions()
+            {
+                Path = "screenshot.png"
+            });
+        }
+    }
+}
